6bcd2163f55c97cfecf2401738550a348f3f7c85
skdiawotjd <skdiawotjd12@naver.com>
2022-06-13 17:23:57 +0900

    Connect Quset to Server

--- Assets/Script/Public/UserInfo.cs
+++ Assets/Script/Public/UserInfo.cs
@@ -1,10 +1,5 @@
 using System.Collections.Generic;
-//
-//
 using UnityEngine;
-//
-//
-using UnityEngine.UI;
 
 public class UserInfo
 {
@@ -14,9 +9,8 @@ public class UserInfo
     private double _userExp;
     private int _gold;
     private int _diamond;
-    private Image _userimage;
 
-    /// <summary>
+    /*/// <summary>
     /// 리스트 0번 : 메인퀘스트
     /// 리스트 1번 : 일일퀘스트
     /// 리스트 2번 : 주간퀘스트
@@ -24,10 +18,8 @@ public class UserInfo
     /// </summary>
     private List<QuestInfo> QuestList;
 
-    /*    private List<Risker> _riskers = new List<Risker>();
-        private List<Equipment> _equipment = new List<Equipment>();*/
 
-    private int _clearMainQuset;
+    private int _clearMainQuset;*/
 
     int[] RiskerStatArray = new int[6];
 
@@ -62,7 +54,6 @@ public class UserInfo
         set
         {
             _userLevel = value;
-            //_userLevel = value;
         }
     }
     public double UserExp
@@ -98,7 +89,7 @@ public class UserInfo
             _diamond = value;
         }
     }
-    public int ClearMainQuset
+    /*public int ClearMainQuset
     {
         get
         {
@@ -108,39 +99,14 @@ public class UserInfo
         {
             _clearMainQuset = value;
         }
-    }
-    /*    public List<Risker> Riskers
-        {
-            get
-            {
-                return _riskers;
-            }
-            set
-            {
-                _riskers = value;
-            }
-        }
-        public List<Equipment> Equipment
-        {
-            get
-            {
-                return _equipment;
-            }
-            set
-            {
-                _equipment = value;
-            }
-        }*/
+    }*/
 
     public Dictionary<string, Risker> RiskerDictionary;
     public Dictionary<string, Equipment> EquipmentDictionary;
     public Dictionary<string, QuestInfo> QuestDictionary;
 
-    // 리스커 클래스의 정보 부족, 장비 클래스의 정보
     public UserInfo(string NewUserCode, string NewUserName, int NewUserLevel, double NewUserExp, int NewGold, int NewDiamond,
-        string NewRisker, string NewEquipment, string NewQuest)
-    /*public UserInfo(string NewUserName, int NewUserLevel, double NewUserExp, int NewGold, int NewDiamond,
-        string NewRisker, string NewEquipment)*/
+        string NewQuest, string NewRisker, string NewEquipment)
     {
         UserCode = NewUserCode;
         UserName = NewUserName;
@@ -149,25 +115,22 @@ public class UserInfo
         Gold = NewGold;
         Diamond = NewDiamond;
 
+        QuestDictionary = new Dictionary<string, QuestInfo>();
         RiskerDictionary = new Dictionary<string, Risker>();
         EquipmentDictionary = new Dictionary<string, Equipment>();
-        QuestDictionary = new Dictionary<string, QuestInfo>();
 
-        // 리스커 저장
-        CreateObject(NewRisker, 1);
-        // 장비 저장
-        CreateObject(NewEquipment, 2);
         // 퀘스트 저장
-        //CreateObject(NewQuest, 3);
+        CreateObject(NewQuest, 1);
+        // 리스커 저장
+        CreateObject(NewRisker, 2);
+        // 장비 저장
+        CreateObject(NewEquipment, 3);
 
     }
 
-    public void SetEquipmentToRisker(string SetRiskerNumber, string EquipmentNumber)
-    {
-        // 리스커안에 있는 stat에 장비의 stat를 더해서 attack등을 set한다
-        RiskerDictionary[SetRiskerNumber].SetEquipment(EquipmentDictionary[EquipmentNumber].EquipmentType, EquipmentNumber);
-    }
-
+    /// <summary>
+    /// SeparatorString - 에서, UntilString - 까지, StringForCount - 를, tem_list - 에 저장
+    /// </summary>
     private void SplitString(string SeparatorString, char UntilString, string StringForCount, ref List<string> tem_list)
     {
         int index = 0;
@@ -185,6 +148,7 @@ public class UserInfo
             {
                 tem_str += StringForCount[index++];
             }
+            //Debug.Log(tem_str);
             tem_list.Add(tem_str);
 
             index = StringForCount.IndexOf(SeparatorString, index);
@@ -192,110 +156,75 @@ public class UserInfo
     }
 
     /// <summary>
-    /// ObjectType : 1 - 리스커, 2 - 장비, 3 - 퀘스트
+    /// ObjectType : 1 - 퀘스트, 2 - 리스커, 3 - 장비
     /// </summary> 
     private void CreateObject(string StringForCount, int ObjectType)
     {
-        //[{"EquipmentNumber":"1100","UserName":"11000","Stat":"100","StatOrder":"0"},{"EquipmentNumber":"1101","UserName":"0","Stat":"100","StatOrder":"0"},{"EquipmentNumber":"1102","UserName":"0","Stat":"200","StatOrder":"0"},{"EquipmentNumber":"1103","UserName":"0","Stat":"300","StatOrder":"0"}]
         List<string> tem_list = new List<string>();
 
         switch (ObjectType)
         { 
             case 1:
-                // [{"RiskerNumber":"11000","RiskerLevel":"1","RiskerExp":"0","ActiveEquipmentNumber":["0","0","0","0","0","0"]}]
-                // 의 "RiskerNumber":"11000","RiskerLevel":"1","RiskerExp":"0" 값 추출
+                // [{"QuestCode":"1","QuestNumber":"2","QuestClear":"3","QuestReward":"4","QuestProcessivity":["5","6","7"]}]에서
+                // "QuestCode":"1","QuestNumber":"2","QuestClear":"3","QuestReward":"4" 값 추출
                 SplitString(":\"", '\"', StringForCount, ref tem_list);
-                //int ActiveEquipmentNumberOrder = 0;
-                // 의 "ActiveEquipmentNumber":["0","0","0","0","0","0"] 값 추출
+                // "QuestProcessivity":["5","6","7"] 값 추출
+                SplitString("[\"", ']', StringForCount, ref tem_list);
+
+                for (int i = 0; i < (tem_list.Count / 5); i++)
+                {
+                    QuestDictionary.Add(tem_list[i * 4], new QuestInfo(int.Parse(string.Format("{0}", tem_list[(i * 4)])), int.Parse(string.Format("{0}", tem_list[(i * 4) + 1])),
+                        int.Parse(string.Format("{0}", tem_list[(i * 4) + 2])), int.Parse(string.Format("{0}", tem_list[(i * 4) + 3])), tem_list[((tem_list.Count / 5) * 4) + i]));
+                }
+
+                break;
+            case 2:
+                // [{"RiskerNumber":"11000","RiskerLevel":"1","RiskerExp":"0","ActiveEquipmentNumber":["0","0","0","0","0","0"]}]에서
+                // "RiskerNumber":"11000","RiskerLevel":"1","RiskerExp":"0" 값 추출
+                SplitString(":\"", '\"', StringForCount, ref tem_list);
+                // "ActiveEquipmentNumber":["0","0","0","0","0","0"] 값 추출
                 SplitString("[\"", ']', StringForCount, ref tem_list);
 
                 for (int i = 0; i < (tem_list.Count / 4); i++)
                 {
-                    //_riskers.Add(new Risker(i, i + 1, (double)(i * 10), i + 1, i + 1, i + 1, i + 1));
-                    //(int NewRiskerNumber, int NewRiskerLevel, double NewRiskerExp, int NewStatStr, int NewStatInt, int NewStatDef, int NewStatAgi)
-                    //RiskerDictionary.Add(i, new Risker(i, i + 1, (double)(i * 10), i + 1, i + 1, i + 1, i + 1, "asd"));
                     RiskerDictionary.Add(tem_list[i * 3], new Risker(tem_list[i * 3], int.Parse(string.Format("{0}", tem_list[(i * 3) + 1])),
                         double.Parse(string.Format("{0}", tem_list[(i * 3) + 2])), tem_list[((tem_list.Count / 4) * 3) + i]));
                 }
                 break;
-            case 2:
-                // [{"EquipmentNumber":"1100","EquipmentType":"0","OwnerNumber":"11000","StatOrder":"0","Stat":"100"}}]
+            case 3:
+                // [{"EquipmentNumber":"1100","EquipmentType":"0","OwnerNumber":"11000","StatOrder":"0","Stat":"100"}}]에서
+                // "EquipmentNumber":"1100","EquipmentType":"0","OwnerNumber":"11000","StatOrder":"0","Stat":"100" 값 추출
                 SplitString(":\"", '\"', StringForCount, ref tem_list);
-
 
                 for (int i = 0; i < (tem_list.Count / 5); i++)
                 {
 
-                    EquipmentDictionary.Add(tem_list[i * 5], new Equipment(tem_list[i * 5], int.Parse(string.Format("{0}", tem_list[(i * 5) + 1])), 
+                    EquipmentDictionary.Add(tem_list[i * 5], new Equipment(tem_list[i * 5], int.Parse(string.Format("{0}", tem_list[(i * 5) + 1])),
                         tem_list[(i * 5) + 2], int.Parse(string.Format("{0}", tem_list[(i * 5) + 3])), int.Parse(string.Format("{0}", tem_list[(i * 5) + 4]))));
-                    
-
-                    //_equipment.Add(new Equipment(i, -1 , i + 1));
-                    //int NewEquipmentNumber, int NewOwerNumber, int NewStat)
-                    if (i == 0)
-                    {
-                        //EquipmentDictionary.Add(i, new Equipment(i, i, i + 1));
-                    }
-                    else
-                    {
-                        //EquipmentDictionary.Add(i, new Equipment(i, -1, i + 1));
-                    }
-
-                    // NewEquipment에 작성된 값 중 NewOwerNumber가 -1이 아니면 소유하는 리스커넘버가 있음
-                    // if문으로 -1이 아니면 조건 걸기
-
-
-
-                    /*if (i == 0)
-                    {
-                        SetEquipmentToRisker(EquipmentDictionary[i].OwerNumber, EquipmentDictionary[i].EquipmentNumber);
-                    }*/
-
-
-
                 }
+
                 break;
-            case 3:
-                // 퀘스트 객체 생성
-                
+            default:
+                Debug.Log("CreateObject함수의 ObjectType이 잘못됨");
                 break;
         }
+
 
 
     }
-    /*public void ActiveEquipStat(int RiskerNumber)
+
+    /// <summary>
+    /// 무기 장착
+    /// </summary>
+    public void SetEquipmentToRisker(string SetRiskerNumber, string EquipmentNumber)
     {
-        for(int EquipmentOrder = 0; EquipmentOrder < RiskerDictionary[RiskerNumber].ActiveEquipmentNumber.Length; EquipmentOrder++)
-        {
-            if(RiskerDictionary[RiskerNumber].ActiveEquipmentNumber[EquipmentOrder] != -1)
-            {
-                Debug.Log("order " + EquipmentOrder);
+        // 리스커안에 있는 stat에 장비의 stat를 더해서 attack등을 set한다
+        RiskerDictionary[SetRiskerNumber].SetEquipment(EquipmentDictionary[EquipmentNumber].EquipmentType, EquipmentNumber);
+    }
 
-                if (EquipmentOrder < 1000)
-                {
-                    Debug.Log("능력치 " + EquipmentDictionary[EquipmentOrder].Stat);
-                    RiskerStatArray[1] += EquipmentDictionary[EquipmentOrder].Stat;
-                }
-                else if (EquipmentOrder < 2000)
-                {
-                    Debug.Log("능력치 " + EquipmentDictionary[EquipmentOrder].Stat);
-                    RiskerStatArray[2] += EquipmentDictionary[EquipmentOrder].Stat;
-                }
-                else if (EquipmentOrder < 3000)
-                {
-                    Debug.Log("능력치 " + EquipmentDictionary[EquipmentOrder].Stat);
-                    RiskerStatArray[3] += EquipmentDictionary[EquipmentOrder].Stat;
-                    RiskerStatArray[4] += EquipmentDictionary[EquipmentOrder].Stat;
-                }
-                else if (EquipmentOrder < 4000)
-                {
-                    Debug.Log("능력치 " + EquipmentDictionary[EquipmentOrder].Stat);
-                    RiskerStatArray[5] += EquipmentDictionary[EquipmentOrder].Stat;
-                }
-            }
-        }
-    }*/
-
+    /// <summary>
+    /// 리스커 공격력/hp/체력/스피드 출력
+    /// </summary>
     public int[] RiskerStat(string RiskerNumber)
     {
         RiskerStatArray[0] = RiskerDictionary[RiskerNumber].RiskerLevel;
--- Assets/Script/Public/UserInfoManager.cs
+++ Assets/Script/Public/UserInfoManager.cs
@@ -8,30 +8,16 @@ public class UserInfoManager : MonoBehaviour
     public ServerManager ServerManager;
 
 
-    private bool Ok = false;
+    private bool _ok = false;
 
     /// <summary>
     /// 0/0/0 - User_Info/Risker/Equipment가 갱신되었는지 확인
     /// 0이면 갱신되지 않음, 1이면 갱신됨
+    /// Index : 0 - User_Info, 1 - Quest , 2 - Risker, 3 - Equipment
     /// </summary>
-    private string Updated = "";
-    private int[] WeatherUpdateData = new int[3] { 0, 0, 0 };
-    /// <summary>
-    /// 1 - User_Info, 2 - Risker, 3 Equipment
-    /// </summary> 
-    /*private int[] WeatherUpdateData
-    {
-        set
-        {
-            _weatherupdatedata = value;
-        }
-        get
-        {
-            return _weatherupdatedata;
-        }
-    }*/
+    private int[] WeatherUpdateData = new int[4] { 0, 0, 0, 0 };
 
-    private string[] _updateduserdata = new string[3];
+    private string[] _updateduserdata = new string[4];
     // ServerManager의 74Line을 위해 사용
     public string this[int index]
     {
@@ -40,7 +26,7 @@ public class UserInfoManager : MonoBehaviour
             if (_updateduserdata[index] != value)
             {
                 WeatherUpdateData[index] = 1;
-                Ok = false;
+                _ok = false;
             }
             _updateduserdata[index] = value;
         }
@@ -61,14 +47,13 @@ public class UserInfoManager : MonoBehaviour
         }
     }
 
-    /*private bool _isChange = true;
-    private bool IsChange
+    public bool Ok
     {
         get
         {
-            return _isChange;
+            return _ok;
         }
-    }*/
+    }
 
     /*// called first
     void OnEnable()
@@ -92,53 +77,31 @@ public class UserInfoManager : MonoBehaviour
 
     public void SetUserData()
     {
-        
         // 1. 유저정보 / 리스커 / 장비 데이터 중 어느 데이터가 갱신되었는지 확인
         for(int i = 0; i < WeatherUpdateData.Length; i++)
         {
             if (WeatherUpdateData[i] == 1)
             {
-                Updated += "1";
+                switch(i)
+                {
+                    case 0:
+                        Debug.Log("User_Info가 갱신됨");
+                        break;
+                    case 1:
+                        Debug.Log("Quest가 갱신됨");
+                        break;
+                    case 2:
+                        Debug.Log("Risker가 갱신됨");
+                        break;
+                    case 3:
+                        Debug.Log("Equipment가 갱신됨");
+                        break;
+                }
             }
-            else
-            {
-                Updated += "0";
-            }
-        }
-
-        // 2. 갱신된 부분에 따라 특정 값 갱신
-        switch(Updated)
-        {
-            case "000":
-                Debug.Log("갱신된 데이터가 없음");
-                break;
-            case "001":
-                Debug.Log("User_Info가 갱신됨");
-                break;
-            case "010":
-                Debug.Log("Risker가 갱신됨");
-                break;
-            case "100":
-                Debug.Log("Equipment가 갱신됨");
-                break;
-            case "011":
-                Debug.Log("User_Info, Risker가 갱신됨");
-                break;
-            case "101":
-                Debug.Log("User_Info, Equipment가 갱신됨");
-                break;
-            case "110":
-                Debug.Log("Risker, Equipment가 갱신됨");
-                break;
-            case "111":
-                Debug.Log("User_Info, Risker, Equipment가 갱신됨");
-                CreateUserInfo();
-                break;
         }
     }
 
     // User_Info, Risker, Equipment가 각각 갱신되는 함수 필요
-
     private void SplitString(string SeparatorString, char UntilString, string StringForCount, ref List<string> tem_list)
     {
         int index = 0;
@@ -162,36 +125,15 @@ public class UserInfoManager : MonoBehaviour
             index = StringForCount.IndexOf(SeparatorString, index);
         }
     }
-    private void CreateUserInfo()
+    public void CreateUserInfo()
     {
         List<string> tem_list = new List<string>();
+        // [{"UserCode":"0000","UserName":"skdiawotjd","UserLevel":"1","UserExp":"1","Gold":"1","Diamond":"1"}]
         SplitString(":\"", '\"', UpdatedUserData[0], ref tem_list);
-        //SplitString("[\"", ']', UpdatedUserData[0], ref tem_list);
-        /* int index = 0;
-         string tem_str = "";
-         List<string> tem_list = new List<string>();
-
-         Debug.Log(UpdatedUserData[0]);
-         for (index = UpdatedUserData[0].IndexOf(":\"", index); index != -1;)
-         {
-             // 한번 사용 후 이전 값을 지우기 위해 초기화
-             tem_str = "";
-             //:"에서 2칸 더 가야 값이므로
-             index += 2;
-             // "가 나올 때 까지 계속 임시 str에 추가 (:"0000")
-             while (UpdatedUserData[0][index] != '\"')
-             {
-                 tem_str += UpdatedUserData[0][index++];
-             }
-             //Debug.Log(tem_str);
-             tem_list.Add(tem_str);
-
-             index = UpdatedUserData[0].IndexOf(":\"", index);
-         }*/
 
         UserInfo = new UserInfo(tem_list[0], tem_list[1], int.Parse(string.Format("{0}", tem_list[2])), double.Parse(string.Format("{0}", tem_list[3])),
             int.Parse(string.Format("{0}", tem_list[4])), int.Parse(string.Format("{0}", tem_list[5])),
-            UpdatedUserData[1], UpdatedUserData[2], "quest");
+            UpdatedUserData[1], UpdatedUserData[2], UpdatedUserData[3]);
 
         /*UserInfo = new UserInfo("skdiawotjd", 1, 10.0f, 100, 200, 
             2, "리스커", 3, "장비");*/
@@ -199,62 +141,11 @@ public class UserInfoManager : MonoBehaviour
         // int NewCountRisker, string NewRisker, int NewCountEquipment, string NewEquipment)
 
         // 서버에서 받은 데이터를 모두 갱신하였으므로 갱신을 판단하는 변수 초기화
-        Updated = "";
         for(int i = 0; i < WeatherUpdateData.Length; i++)
         {
             WeatherUpdateData[i] = 0;
         }
 
-        Ok = true;
+        _ok = true;
     }
-
-    public bool IsReady()
-    {
-        return Ok;
-    }
-
-    /*public void SetChangeEquipment(int RiskerNumber, int ToBeEuipmentNumber)
-    {
-        for (int i = 0; i < UserInfo.Riskers.Count; i++)
-        {
-            if (RiskerNumber == UserInfo.Riskers[i].RiskerNumber)
-            {
-                for (int k = 0; k < UserInfo.Equipment.Count; k++)
-                {
-                    if (ToBeEuipmentNumber == UserInfo.Equipment[k].EquipmentNumber)
-                    {
-                        for (int m = 0; m < UserInfo.Riskers[i].ActiveEquipment.Count; m++)
-                        {
-                            Debug.Log(UserInfo.Equipment[k].StatOrder + " == " + UserInfo.Riskers[i].ActiveEquipment[m].StatOrder);
-                            if (UserInfo.Equipment[k].StatOrder == UserInfo.Riskers[i].ActiveEquipment[m].StatOrder)
-                            {
-                                Equipment TemEquipment = UserInfo.Equipment[k];
-                                UserInfo.Equipment[k] = UserInfo.Riskers[i].ActiveEquipment[m];
-                                UserInfo.Riskers[i].ActiveEquipment[m] = TemEquipment;
-
-                                BeEuipment = UserInfo.Riskers[i].ActiveEquipment[m];
-                                ToBeEuipment = UserInfo.Equipment[k];
-                                break;
-                            }
-
-                        }
-                        break;
-                    }
-                }
-                break;
-            }
-        }
-    }*/
-    /*public void ApplyEquipment()
-    {
-        Equipment TemEquipment = ToBeEuipment;
-        ToBeEuipment = BeEuipment;
-        BeEuipment = TemEquipment;
-    }*/
 }
-
-/*
-UserInfoManager는 처음 시작 시 서버에서 유저의 정보를 받아와 저장한다 - 이는 각 씬에서 UI로 유저의 정보를 보여줄 때 사용
-
- 
-*/
